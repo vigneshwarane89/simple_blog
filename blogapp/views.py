ac208f4078e71427ab19733def1b13b8f600e571
vigneshwarane <vigneshwarane89@gmail.com>
2020-06-25 17:50:08 +0530

    First step
    A class BlogArticle is created and several changes were made in
    settings.py views.py urls.py and a new folder was createdd -templates-
    and a html file with it as a template and it returns a simple hi!
    message.

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	#return HttpResponse("HI!!!")
+	return render(request,'base.html',{'testvar':'h1!NEW'})
