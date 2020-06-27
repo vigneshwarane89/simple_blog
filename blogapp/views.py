d93b35eb56c7f69ee25d51039319d5b75c7ae13b
vigneshwarane89 <vigneshwarane89@gmail.com>
2020-06-27 10:28:10 +0530

    BlogArticle class is created in views.py and a list of blogarticle objects is stored in blogs and rendered. a object is created using shell.

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from blogapp.models import BlogArticle
+from django.contrib.auth.models import User
 # Create your views here.
+"""u=(User.objects.all())
+testuser=u[0]
+simple=BlogArticle()
+simple.title="Sample"
+simple.blog_content="This is a sample blog"
+simple.author=testuser
+
+"""
+
+blogs=BlogArticle.objects.all()
+
+
 def index(request):
 	#return HttpResponse("HI!!!")
-	return render(request,'base.html',{'testvar':'h1!NEW'})
+	return(render(request,'main.html',{"blogs":blogs}))
